9143ff0c405ae96bbb02822fe62554192588ae5b
Serhii Provotorov <sergius.sport@gmail.com>
2024-01-09 08:35:03 +0100

    add variables and filter for dtaframe

--- MSA_project.py
+++ MSA_project.py
@@ -1,5 +1,21 @@
 import pandas as pd
 
 
-df = pd.read_csv("C:/MSA_project/data/PH59_bot_Comp.csv")
+path = "C:/MSA_project/data/PH59_bot_Comp.csv"
+df = pd.read_csv(path)
+
+columns = ["Index", "Location Name", "ModuleID", "OffsetX", "OffsetY", "Comp.Result", 'SizeX', 'SizeY']
+modules = [1, 2, 3, 4, 5]
+components = ["C1"]
+
+filt = (df["ModuleID"].isin(modules)) & (df["Location Name"].isin(components))
+
+filtered_data = df.loc[filt, columns]
+
+
 df.to_excel("PH59_data_fo_MSA.xlsx", index=False)
+
+#some = df.columns
+#print(type(some))
+#for i in some:
+#    print(i)
