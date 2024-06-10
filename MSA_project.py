3ee0b995724a6a8d8bd62755fcff9ee7dee46a9e
Serhii Provotorov <K90006169@kimballelectronics.com>
2024-06-10 15:53:55 +0200

    create variables and loops to filter and add filtered data from csv file to new data frame and export it to excel file

--- MSA_project.py
+++ MSA_project.py
@@ -1,21 +1,26 @@
 import pandas as pd
 
+#take path to the data
+path_top = "C:\\Users\\K90006169\\MSA_project\\PH71_top_Comp.csv" #"C:/MSA_project/data/PH59_bot_Comp.csv"
+path_bot = "C:\\Users\\K90006169\\MSA_project\\PH71_bot_Comp.csv"
 
-path = "C:/MSA_project/data/PH59_bot_Comp.csv"
-df = pd.read_csv(path)
+#read the data
+df = pd.read_csv(path_top)
 
-columns = ["Index", "Location Name", "ModuleID", "OffsetX", "OffsetY", "Comp.Result", 'SizeX', 'SizeY']
-modules = [1, 2, 3, 4, 5]
-components = ["C1"]
+#define variables
+df_columns = ["OffsetX", "OffsetY"]
+modules = 5
+components_top = ["R201", "R205"]
+components_bot = ["R303", "R510"]
+msa_data = pd.DataFrame(columns=[f"TOP_{components_top[0]}_X", f"TOP_{components_top[1]}_Y"])
 
-filt = (df["ModuleID"].isin(modules)) & (df["Location Name"].isin(components))
+for j in components_top:
+    
 
-filtered_data = df.loc[filt, columns]
-#something to commit
 
-df.to_excel("PH59_data_fo_MSA.xlsx", index=False)
+    filtered_data = pd.DataFrame(columns=df_columns)
+    for i in range(modules):
+        filt = (df["ModuleID"] == i + 1) & (df["Location Name"] == j)
+        filtered_data =pd.concat([filtered_data, df.loc[filt, df_columns].iloc[:9]], ignore_index=True)
 
-#some = df.columns
-#print(type(some))
-#for i in some:
-#    print(i)
+msa_data.to_excel("PH71TOP_data_fo_MSA.xlsx", index=False) #("PH59_data_fo_MSA.xlsx", index=False)
