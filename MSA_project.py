db1ac85635f170fdc25335ed2913c555f0ac25f3
Serhii Provotorov <sergius.sport@gmail.com>
2024-06-11 15:54:18 +0200

    To MSA_project.py add comments and functionality to filter readed data, create with these data two dataframes and export it to excel file. In Application_GUI.py create new class AppController to handle the signal and the slots.

--- MSA_project.py
+++ MSA_project.py
@@ -1,26 +1,79 @@
 import pandas as pd
 
+def read_data(path):
+    
+    df = pd.read_csv(path)
+    return df
+
 #take path to the data
-path_top = "C:\\Users\\K90006169\\MSA_project\\PH71_top_Comp.csv" #"C:/MSA_project/data/PH59_bot_Comp.csv"
+path_top = "C:\\Users\\K90006169\\MSA_project\\PH71_top_Comp.csv" 
 path_bot = "C:\\Users\\K90006169\\MSA_project\\PH71_bot_Comp.csv"
 
 #read the data
-df = pd.read_csv(path_top)
+df_top = pd.read_csv(path_top)
+df_bot = pd.read_csv(path_bot)
 
 #define variables
 df_columns = ["OffsetX", "OffsetY"]
 modules = 5
 components_top = ["R201", "R205"]
 components_bot = ["R303", "R510"]
-msa_data = pd.DataFrame(columns=[f"TOP_{components_top[0]}_X", f"TOP_{components_top[1]}_Y"])
+columns_for_msa_data = {
+    'Operator': [1 for i in range(45)],
+    'Part': [i+1 for i in range(5) for j in range(9)]
+}
+msa_data = pd.DataFrame(columns_for_msa_data)
 
-for j in components_top:
-    
-
-
+for i in components_top:
+    #create a temporary dataframe
     filtered_data = pd.DataFrame(columns=df_columns)
-    for i in range(modules):
-        filt = (df["ModuleID"] == i + 1) & (df["Location Name"] == j)
-        filtered_data =pd.concat([filtered_data, df.loc[filt, df_columns].iloc[:9]], ignore_index=True)
 
-msa_data.to_excel("PH71TOP_data_fo_MSA.xlsx", index=False) #("PH59_data_fo_MSA.xlsx", index=False)
+    for j in range(modules):
+        #create filter
+        filt = (df_top["ModuleID"] == j + 1) & (df_top["Location Name"] == i)
+        #filtered data from the .csv file and add it to the temporary dataframe
+        filtered_data = pd.concat([filtered_data, df_top.loc[filt, df_columns].iloc[:9]], ignore_index=True)
+
+    column_mapping = {
+        df_columns[0]: f"TOP_{i}_X",
+        df_columns[1]: f"TOP_{i}_Y"
+    }
+    #rename the columns and update the values of the temporary dataframe
+    filtered_data = filtered_data.rename(columns=column_mapping).apply(lambda x: x/1000)
+    #concatenate the MSA dataframe and the temporary dataframe with renaming columns of the temporary dataframe
+    msa_data = pd.concat([msa_data, filtered_data], axis=1)
+
+for i in components_bot:
+    #create a temporary dataframe
+    filtered_data = pd.DataFrame(columns=df_columns)
+
+    for j in range(modules):
+        #create filter
+        filt = (df_bot["ModuleID"] == j + 1) & (df_bot["Location Name"] == i)
+        #filtered data from the .csv file and add it to the temporary dataframe
+        filtered_data = pd.concat([filtered_data, df_bot.loc[filt, df_columns].iloc[:9]], ignore_index=True)
+
+    column_mapping = {
+        df_columns[0]: f"BOT_{i}_X",
+        df_columns[1]: f"BOT_{i}_Y"
+    }
+    #rename the columns and update the values of the temporary dataframe
+    filtered_data = filtered_data.rename(columns=column_mapping).apply(lambda x: x/1000)
+    #concatenate the MSA dataframe and the temporary dataframe
+    msa_data = pd.concat([msa_data, filtered_data], axis=1)
+
+#create a dataframe for measurement tolerance
+components = components_top + components_bot
+n = len(components)
+columns_with_data = {
+    'Desygnator': components,
+    'Obudowa': ['none' for i in range(n)],
+    'Tolerancja X': [0 for i in range(n)],
+    'Tolerancja Y': [0 for i in range(n)]
+}
+tolerance_data = pd.DataFrame(columns_with_data)
+
+#export MSA and tolerance dataframes to the Excel
+with pd.ExcelWriter('MSA_data.xlsx', engine='xlsxwriter') as writer:
+    msa_data.to_excel(writer, sheet_name='Sheet1', startrow=0, startcol=0, index=False)
+    tolerance_data.to_excel(writer, sheet_name='Sheet1', startrow=0, startcol=len(msa_data.columns) + 2, index=False)
--- application_GUI.py
+++ application_GUI.py
@@ -16,67 +16,79 @@ from PyQt6.QtWidgets import (
 from PyQt6.QtGui import QPixmap
 
 
-WINDOW_WIDTH = 1000
-WINDOW_HEIGHT = 400
+WINDOW_WIDTH = 1100
+WINDOW_HEIGHT = 250
 LINE_HEIGHT = 50
+BUTTON_WIDTH = 100
+BUTTON_HEIGHT = 25
+
 
 class AppGUI(QMainWindow):
     """Application's GUI"""
     def __init__(self):
         super().__init__()
         self.setWindowTitle("MSA data processing")
-        #self.setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT)
+        self.setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT)
         self.main_layout = QHBoxLayout()
         central_widget = QWidget(self)
         central_widget.setLayout(self.main_layout)
         self.setCentralWidget(central_widget)
-        self.main_layout.setContentsMargins(20, 20, 20, 20)
+        self.main_layout.setContentsMargins(15, 15, 15, 15)
 
         self.create_input_boxes()
         self.main_layout.addSpacing(20)
         self.create_image()
-        #self.main_layout.setSpacing(20)
+        self.main_layout.setSpacing(20)
 
     def create_input_boxes(self):
         # Create grid layout
         self.grid_layout = QGridLayout()
-        self.grid_layout.setSpacing(10)
+        self.grid_layout.setSpacing(11)
         
         # Add Input Boxes in the first column
-        label1 = QLabel("Input data for the TOP side:")
+        label1 = QLabel("Select CSV file for the TOP side:")
         self.input_box1 = QLineEdit(self)
 
-        label2 = QLabel("Input list of the designators for the TOP side:")
+        label2 = QLabel("Enter designators for the TOP side separated by a comma:")
         self.input_box2 = QLineEdit(self)
 
-        label3 = QLabel("Input data for the BOT side:")
+        label3 = QLabel("Select CSV file for the BOT side:")
         self.input_box3 = QLineEdit(self)
 
-        label4 = QLabel("Input list of the designators for the BOT side:")
+        label4 = QLabel("Enter designators for the BOT side separated by a comma:")
         self.input_box4 = QLineEdit(self)
 
         self.grid_layout.addWidget(label1, 0, 0)
         self.grid_layout.addWidget(self.input_box1, 0, 1)
 
         self.grid_layout.addWidget(label2, 1, 0)
-        self.grid_layout.addWidget(self.input_box2, 1, 1, 1, 2)
+        self.grid_layout.addWidget(self.input_box2, 1, 1)#, 1, 2
 
         self.grid_layout.addWidget(label3, 2, 0)
         self.grid_layout.addWidget(self.input_box3, 2, 1)
 
         self.grid_layout.addWidget(label4, 3, 0)
-        self.grid_layout.addWidget(self.input_box4, 3, 1, 1, 2)
+        self.grid_layout.addWidget(self.input_box4, 3, 1)#, 1, 2
 
         # Add Buttons in the second column
         self.button1 = QPushButton("SEARCH", self)
+        self.button1.setFixedSize(BUTTON_WIDTH, BUTTON_HEIGHT)
         self.button2 = QPushButton("SEARCH", self)
-        button3 = QPushButton("CREATE", self)
-        button4 = QPushButton("CLEAR", self)
+        self.button2.setFixedSize(BUTTON_WIDTH, BUTTON_HEIGHT)
+        self.button3 = QPushButton("CREATE EXCEL", self)
+        self.button3.setFixedSize(BUTTON_WIDTH, BUTTON_HEIGHT)
+        button4 = QPushButton("CLEAR DATA", self)
+        button4.setFixedSize(BUTTON_WIDTH, BUTTON_HEIGHT)
+
+        containerWidget = QWidget()
+        containerLayout = QHBoxLayout()
+        containerLayout.addWidget(self.button3)
+        containerLayout.addWidget(button4)
+        containerWidget.setLayout(containerLayout)
 
         self.grid_layout.addWidget(self.button1, 0, 2)
         self.grid_layout.addWidget(self.button2, 2, 2)
-        self.grid_layout.addWidget(button3, 4, 1)
-        self.grid_layout.addWidget(button4, 4, 2)
+        self.grid_layout.addWidget(containerWidget, 4, 1)
 
         self.main_layout.addLayout(self.grid_layout)
 
@@ -91,10 +103,16 @@ class AppGUI(QMainWindow):
         self.pattern_image.setPixmap(pixmap)
         self.main_layout.addWidget(self.pattern_image)
 
+
+class AppController:
+    def __init__(self, view):
+        self.view = view
+        self.connect_signals_and_slots()
+
     def select_file(self):
         #Get the button that triggered the event
-        sender_button = self.sender()
-        options = QFileDialog.Options()
+        sender_button = self.view.sender()
+        options = QFileDialog.Option(0)
         #Show the file dialog to select a file
         file_dialog = QFileDialog()
         file_dialog.setNameFilter("CSV Files (*.csv)")
@@ -105,15 +123,22 @@ class AppGUI(QMainWindow):
             selected_files = file_dialog.selectedFiles()
             if selected_files:
                 file_path = selected_files[0]
-                if sender_button == self.search_top_csv:
-                    self.csv_top_path.setText(file_path)
-                elif sender_button == self.search_bot_csv:
-                    self.csv_bot_path.setText(file_path)
+                if sender_button == self.view.button1:
+                    self.view.input_box1.setText(file_path)
+                elif sender_button == self.view.button2:
+                    self.view.input_box3.setText(file_path)
+
+    def connect_signals_and_slots(self):
+        self.view.button1.clicked.connect(self.select_file)
+        self.view.button2.clicked.connect(self.select_file)
 
 def main():
     app = QApplication([])
     appGui = AppGUI()
     appGui.show()
+    appController = AppController(
+        view=appGui
+    )
     sys.exit(app.exec())
 
 if __name__ == "__main__":
