99ed98d332d9e2a6eeb689e320f422f1c2de899a
Serhii Provotorov <sergius.sport@gmail.com>
2024-06-13 10:35:27 +0200

    .gitignore was changed, added to the repository .csv and .xlsx files with data, in application_GUI.py a create_input_boxes() function was updated and created clear_data() function

--- application_GUI.py
+++ application_GUI.py
@@ -16,12 +16,11 @@ from PyQt6.QtWidgets import (
 from PyQt6.QtGui import QPixmap
 
 
-WINDOW_WIDTH = 1100
-WINDOW_HEIGHT = 250
+WINDOW_WIDTH = 750
+WINDOW_HEIGHT = 500
 LINE_HEIGHT = 50
 BUTTON_WIDTH = 100
 BUTTON_HEIGHT = 25
-
 
 class AppGUI(QMainWindow):
     """Application's GUI"""
@@ -29,11 +28,11 @@ class AppGUI(QMainWindow):
         super().__init__()
         self.setWindowTitle("MSA data processing")
         self.setFixedSize(WINDOW_WIDTH, WINDOW_HEIGHT)
-        self.main_layout = QHBoxLayout()
+        self.main_layout = QVBoxLayout()
         central_widget = QWidget(self)
         central_widget.setLayout(self.main_layout)
         self.setCentralWidget(central_widget)
-        self.main_layout.setContentsMargins(15, 15, 15, 15)
+        self.main_layout.setContentsMargins(20, 20, 20, 20)
 
         self.create_input_boxes()
         self.main_layout.addSpacing(20)
@@ -41,49 +40,46 @@ class AppGUI(QMainWindow):
         self.main_layout.setSpacing(20)
 
     def create_input_boxes(self):
-        # Create grid layout
+        #create list of label texts
+        texts = [
+            "Select CSV file for the TOP side:", 
+            "Enter designators for the TOP side separated by a comma:", 
+            "Select CSV file for the BOT side:", 
+            "Enter designators for the BOT side separated by a comma:"
+            ]
+
+        #define the number of input boxes
+        self.input_boxes_names = [[f"label{i + 1}", f"input{i + 1}"]for i in range(len(texts))]
+
+        #create a grid layout
         self.grid_layout = QGridLayout()
-        self.grid_layout.setSpacing(11)
-        
-        # Add Input Boxes in the first column
-        label1 = QLabel("Select CSV file for the TOP side:")
-        self.input_box1 = QLineEdit(self)
+        self.grid_layout.setSpacing(20)
 
-        label2 = QLabel("Enter designators for the TOP side separated by a comma:")
-        self.input_box2 = QLineEdit(self)
+        #define the input boxes and place them in the layout
+        self.input_boxes = {}
+        for row, input_box in enumerate(self.input_boxes_names):
+            for column, element in enumerate(input_box):
+                if column == 0:
+                    self.input_boxes[element] = QLabel(texts[row])
+                    self.grid_layout.addWidget(self.input_boxes[element], row, column)
+                else:
+                    self.input_boxes[element] = QLineEdit(self)
+                    self.grid_layout.addWidget(self.input_boxes[element], row, column)
 
-        label3 = QLabel("Select CSV file for the BOT side:")
-        self.input_box3 = QLineEdit(self)
-
-        label4 = QLabel("Enter designators for the BOT side separated by a comma:")
-        self.input_box4 = QLineEdit(self)
-
-        self.grid_layout.addWidget(label1, 0, 0)
-        self.grid_layout.addWidget(self.input_box1, 0, 1)
-
-        self.grid_layout.addWidget(label2, 1, 0)
-        self.grid_layout.addWidget(self.input_box2, 1, 1)#, 1, 2
-
-        self.grid_layout.addWidget(label3, 2, 0)
-        self.grid_layout.addWidget(self.input_box3, 2, 1)
-
-        self.grid_layout.addWidget(label4, 3, 0)
-        self.grid_layout.addWidget(self.input_box4, 3, 1)#, 1, 2
-
-        # Add Buttons in the second column
+        #create and place the buttons in the layout
         self.button1 = QPushButton("SEARCH", self)
         self.button1.setFixedSize(BUTTON_WIDTH, BUTTON_HEIGHT)
         self.button2 = QPushButton("SEARCH", self)
         self.button2.setFixedSize(BUTTON_WIDTH, BUTTON_HEIGHT)
         self.button3 = QPushButton("CREATE EXCEL", self)
         self.button3.setFixedSize(BUTTON_WIDTH, BUTTON_HEIGHT)
-        button4 = QPushButton("CLEAR DATA", self)
-        button4.setFixedSize(BUTTON_WIDTH, BUTTON_HEIGHT)
+        self.button4 = QPushButton("CLEAR DATA", self)
+        self.button4.setFixedSize(BUTTON_WIDTH, BUTTON_HEIGHT)
 
         containerWidget = QWidget()
         containerLayout = QHBoxLayout()
         containerLayout.addWidget(self.button3)
-        containerLayout.addWidget(button4)
+        containerLayout.addWidget(self.button4)
         containerWidget.setLayout(containerLayout)
 
         self.grid_layout.addWidget(self.button1, 0, 2)
@@ -124,13 +120,20 @@ class AppController:
             if selected_files:
                 file_path = selected_files[0]
                 if sender_button == self.view.button1:
-                    self.view.input_box1.setText(file_path)
+                    self.view.input_boxes["input1"].setText(file_path)
                 elif sender_button == self.view.button2:
-                    self.view.input_box3.setText(file_path)
+                    self.view.input_boxes["input3"].setText(file_path)
+
+    def clear_data(self):
+        #clear data in the input boxes
+        for key in self.view.input_boxes:
+            if key.find("input") != -1:
+                self.view.input_boxes[key].clear()
 
     def connect_signals_and_slots(self):
         self.view.button1.clicked.connect(self.select_file)
         self.view.button2.clicked.connect(self.select_file)
+        self.view.button4.clicked.connect(self.clear_data)
 
 def main():
     app = QApplication([])
